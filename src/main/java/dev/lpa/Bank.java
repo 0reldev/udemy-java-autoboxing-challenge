5cdad5f48df6d4d5634556ca9f94b17f527816b7
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-22 14:36:40 +0200

    Second part of the resolution

--- src/main/java/dev/lpa/Bank.java
+++ src/main/java/dev/lpa/Bank.java
@@ -29,4 +29,33 @@ public class Bank {
             System.out.println("New customer add: " +  customer);
         }
     }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
+    }
+
+    public void addTransaction(String name, double transactionAmount) {
+
+        Customer customer = getCustomer(name);
+        if (customer != null) {
+            customer.transactions().add(transactionAmount);
+        }
+    }
+
+    public void printStatement(String customerName) {
+        Customer customer = getCustomer(customerName);
+        if (customer == null) {
+            return;
+        }
+        System.out.println("-".repeat(30));
+        System.out.println("Customer name: " + customer.name());
+        System.out.println("Transactions:");
+        for (double d : customer.transactions()) {  // unboxing
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+        }
+    }
 }
--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -6,5 +6,35 @@ public class Main {
 
         Customer bob = new Customer("Bob S.", 1000.0);
         System.out.println(bob);    // Customer[name=BOB S., transactions=[1000.0]]
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A.", 500.0);
+        System.out.println(bank);
+//          Customer (Jane A.) wasn't found
+//          New customer add: Customer[name=JANE A., transactions=[500.0]]
+//          Bank{name='Chase', customers=[Customer[name=JANE A., transactions=[500.0]]]}
+
+        bank.addTransaction("Jane A.", -10.25);
+        bank.addTransaction("Jane A.", -75.01);
+        bank.printStatement("Jane A.");
+//        ------------------------------
+//           Customer name: JANE A.
+//           Transactions:
+//           $    500,00 (credit)
+//           $    -10,25 (debit)
+//           $    -75,01 (debit)
+
+        bank.addNewCustomer("Bob S.", 25.0);
+        bank.addTransaction("Bob S.", 1000.0);
+        bank.printStatement("Bob S.");
+//          Customer (Bob S.) wasn't found
+//          New customer add: Customer[name=BOB S., transactions=[25.0]]
+//          ------------------------------
+//          Customer name: BOB S.
+//          Transactions:
+//          $     25,00 (credit)
+//          $   1000,00 (credit)
     }
+
+
 }
